0c3028041f338bde07289dab0164b29093797d1b
pabaep <55613591+pabaep@users.noreply.github.com>
2021-01-18 18:48:04 +0900

    Update Q1000.java

--- done/Q1000.java
+++ done/Q1000.java
@@ -4,8 +4,8 @@ public class Q1000 {
 
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
-		int a = s.nextInt(); //동전 종류 수
-		int b = s.nextInt(); //결과 얻고 싶은 금액
+		int a = s.nextInt(); 
+		int b = s.nextInt(); 
 		
 		System.out.println(a+b);
 
