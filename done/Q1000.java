2f7879b5a5cdae6e941caea7ac3b3752ef1bc684
pabaep <55613591+pabaep@users.noreply.github.com>
2021-01-18 18:47:51 +0900

    Create Q1000.java

---
+++ done/Q1000.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Q1000 {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int a = s.nextInt(); //동전 종류 수
+		int b = s.nextInt(); //결과 얻고 싶은 금액
+		
+		System.out.println(a+b);
+
+	}
+
+}
