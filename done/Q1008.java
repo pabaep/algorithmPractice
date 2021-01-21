8a88554ef1dd76fbc3a7daef768cc6eaf9eede16
pabaep <55613591+pabaep@users.noreply.github.com>
2021-01-21 22:53:42 +0900

    Create Q1008.java

---
+++ done/Q1008.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Q1008 {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		double a = s.nextInt();
+		double b = s.nextInt();
+		double result = a/b;
+		
+		System.out.println(result);
+
+	}
+
+}
