b671c1f80bfb22f82006678c4ba810edbcd6b634
pabaep <55613591+pabaep@users.noreply.github.com>
2021-01-21 22:46:21 +0900

    Create Q10998.java

---
+++ done/Q10998.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Q10998 {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int a = s.nextInt();
+		int b = s.nextInt();
+		
+		System.out.println(a*b);
+
+	}
+
+}
