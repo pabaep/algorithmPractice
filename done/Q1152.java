bc0191d80d0ed16a757d58b11174f6a73e9c0d11
pabaep <55613591+pabaep@users.noreply.github.com>
2021-03-05 23:20:53 +0900

    Create Q1152.java

---
+++ done/Q1152.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.*;
+
+public class Q1152 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String s = br.readLine();
+		StringTokenizer st = new StringTokenizer(s);
+		int n =st.countTokens();
+
+		System.out.println(n);
+	}
+
+}
