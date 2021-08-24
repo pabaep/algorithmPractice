1b04ab2d97f99ef6fcf02d15d5ae7644c8779b66
pabaep <55613591+pabaep@users.noreply.github.com>
2021-08-24 22:54:56 +0900

    Create stack&queue_2.java

---
+++ programmers/stack&queue_2.java
@@ -0,0 +1,25 @@
+//프린터
+import java.util.*;
+class Solution {
+    public int solution(int[] priorities, int location) {
+        int answer = 0;
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        for(int i = 0;i<priorities.length;i++){
+            pq.add(priorities[i]);
+        }
+        while(!pq.isEmpty()){
+            for(int i = 0;i<priorities.length;i++){
+                if(pq.peek() == priorities[i]){
+                    pq.poll();
+                    answer++;
+                    if(location == i){
+                        pq.clear();
+                        break;
+                    }
+                }
+            }
+        }
+        return answer;
+    }
+    
+}
