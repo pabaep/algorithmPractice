a36b3c43acf8df736a5fbb33c3ace18de26d51d2
pabaep <55613591+pabaep@users.noreply.github.com>
2021-08-27 19:46:32 +0900

    Update heap_1.java

--- programmers/heap_1.java
+++ programmers/heap_1.java
@@ -1,4 +1,4 @@
-//우선순위 큐 이용하기
+// 더 맵게
 
 import java.util.*;
 
@@ -7,10 +7,10 @@ class Solution {
         int answer = 0;
         PriorityQueue<Integer> q = new PriorityQueue();
         for(int i = 0;i<scoville.length;i++){
-            q.add(scoville[i]);
+            q.add(scoville[i]); //우선순위 큐 안에 스코빌지수 배열 넣음
         }
-        while(allPass(q,K)==false){
-            if(q.size()==1){
+        while(allPass(q,K)==false){ //조건에 부합하지 않는다면
+            if(q.size()==1){ //음식이 하나 뿐이라면 -1 리턴
                 answer = -1;
                 break;
             }
@@ -26,6 +26,7 @@ class Solution {
         return answer;
     }
     
+    //전체 음식이 스코빌 조건에 부합하는지 확인하는 함수
     public boolean allPass(PriorityQueue q, int K) {
         int num = (int)q.peek();
         if(num >= K){
