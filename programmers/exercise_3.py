4ce3e515c2a05c67ee7ecb70b6c9ce7ce9ba67bb
Sewon Hwang <55613591+pabaep@users.noreply.github.com>
2022-01-13 13:43:53 +0900

    Create exercise_3.py

---
+++ programmers/exercise_3.py
@@ -0,0 +1,8 @@
+#연습문제 서울에 있는 김서방 찾기
+def solution(seoul):
+    answer = ''
+    for i in range(len(seoul)):
+        if(seoul[i]=="Kim"):
+            answer = "김서방은 "+str(i)+"에 있다"
+            
+    return answer
