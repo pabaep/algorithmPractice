f2c6ee4c100ac012bfe173a210beb1506f1358a3
Sewon Hwang <55613591+pabaep@users.noreply.github.com>
2022-01-18 23:04:06 +0900

    Create exercise_11.py

---
+++ programmers/exercise_11.py
@@ -0,0 +1,8 @@
+#연습문제 행렬의 덧셈
+def solution(arr1, arr2):
+    answer = [[0 for i in range(len(arr1[0]))]for j in range(len(arr1))]
+    for i in range(len(arr1)):
+        for j in range(len(arr1[0])):
+            answer[i][j] = arr1[i][j] + arr2[i][j]
+
+    return answer
